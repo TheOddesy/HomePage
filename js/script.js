9deb5f66cfb3cfa5cf360f496e6dfb0871439282
Nicolas Salas <nicolassalas@Nicolass-MBP.lan>
2021-01-28 12:20:00 +0100

    Horizontal scroll bar implemented. Better structure for header with better containers

--- js/script.js
+++ js/script.js
@@ -15,3 +15,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
     document.documentElement.setAttribute("data-theme", switchToTheme);
   }
 });
+
+
+
+
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
