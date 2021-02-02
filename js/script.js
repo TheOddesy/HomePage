af287c328717368480b186745d4d452f748a42b0
Nicolas Salas <nicolassalas@Nicolass-MBP.lan>
2021-02-02 14:31:04 +0100

    Optimized for mobile experience but its not completely done yet. Changed the drop down menu button to be represented by fa-bars instead of a character.

--- js/script.js
+++ js/script.js
@@ -34,11 +34,11 @@ function dropDownMenu() {
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+  if (!event.target.matches('.dropbtn-part')) {
+    const dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
+      const openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
       }
